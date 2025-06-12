54d4cbc63a2a8bdbc7268f2ee8619fff5dfeb9da
Christian Murua <cristianmurua.18@gmail.com>
2025-06-12 12:46:25 -0300

    acomodo principal e interceptor

--- juego_cartas/lib/features/principal/config/principal_get_it.dart
+++ juego_cartas/lib/features/principal/config/principal_get_it.dart
@@ -1,8 +1,9 @@
 import 'package:get_it/get_it.dart';
-import 'package:juego_cartas/features/login/data/repositories/login_repository_impl.dart';
-import 'package:juego_cartas/features/login/domain/repositories/login_repository.dart';
-import 'package:juego_cartas/features/login/domain/use_cases/get_autorizacion.dart';
-import 'package:juego_cartas/features/login/domain/use_cases/sign_up.dart';
+import 'package:juego_cartas/features/principal/data/repositories/principal_repository_impl.dart';
+import 'package:juego_cartas/features/principal/domain/repositories/principal_repository.dart';
+import 'package:juego_cartas/features/principal/domain/use_cases/get_datos.dart';
+import 'package:juego_cartas/features/principal/domain/use_cases/get_mazos.dart';
+import 'package:juego_cartas/features/principal/domain/use_cases/registro_torneo.dart';
 
 void principalConfigure() {
   //SERIA ASINCRONO? NO
@@ -13,18 +14,21 @@ void principalConfigure() {
   //GetIt.instance.registerLazySingleton<ApiService>(() => ApiService(dioClient));
 
   //Repository y su implementacion
-  GetIt.instance.registerLazySingleton<LoginRepository>(
-    () => LoginRepositoryImpl(apiService: GetIt.instance.get()),
+  GetIt.instance.registerLazySingleton<PrincipalRepository>(
+    () => PrincipalRepositoryImpl(apiService: GetIt.instance.get()),
   );
 
   //Use cases
   GetIt.instance.registerSingleton(
-    GetAutorizacionUseCase(repository: GetIt.instance.get()),
+    GetDatosUseCase(repository: GetIt.instance.get()),
   );
 
   GetIt.instance.registerSingleton(
-    SignUpUseCase(repository: GetIt.instance.get()),
+    GetMazosUseCase(repository: GetIt.instance.get()),
   );
 
+  GetIt.instance.registerSingleton(
+    InscripcionTorneoUseCase(repository: GetIt.instance.get()),
+  );
   //DataSources si tuviera
 }
